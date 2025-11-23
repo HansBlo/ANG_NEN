f834ca0b36967148dc63b546b5850b051a8703ef
HansBlo <blochinge@gmail.com>
2025-11-23 16:05:06 +0100

    Delete ang-nen-9423583e172ffad12a7a02cdff61615822aa129b/ang-nen-9423583e172ffad12a7a02cdff61615822aa129b directory

--- ang-nen-9423583e172ffad12a7a02cdff61615822aa129b/ang-nen-9423583e172ffad12a7a02cdff61615822aa129b/frontend/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import {TableComponent} from './table-component/table-component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: TableComponent,
-  }
-];
